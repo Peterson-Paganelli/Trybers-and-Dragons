d81b9acc230da170eec727a4edda9fff1d8b8e18
Peterson <petersonpaganelli@outlook.com>
2023-02-23 14:23:33 -0300

    feat: add a pvp class

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,23 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(
+    private _player1: Fighter,
+    private _player2: Fighter,
+  ) {
+    super(_player1);
+    super.fight();
+  }
+
+  fight(): number {
+    while (this._player1.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this._player1.attack(this._player2);
+      this._player1.attack(this._player1);
+    }
+    if (this._player1.lifePoints === -1) return -1;
+    return 1;
+  }
+}
+
+export default PVP;
